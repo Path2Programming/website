e3dc62d625470f8758646f2c7519c4ab497406c8
shreshth <shreshthsrivastava2@gmail.com>
2020-11-29 22:51:14 -0500

    fixed error so that src folder can now be named client

---
+++ client/app/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Eastern Kentucky Coding</h1>
+        <p>Link to google form</p>
+      </div>
+    );
+  }
+}
+
+export default App;
