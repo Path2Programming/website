99d0494710c17914ee6b764fdcc0170b3febcdee
AdiG <adigollamudi@gmail.com>
2020-11-29 22:07:31 -0500

    merging issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <h1>Eastern Kentucky Coding</h1>
+      <p>Link to google form</p>
+    </div>
+  );
 }
 
 export default App;
