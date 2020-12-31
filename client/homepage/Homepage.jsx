2b5fe95a8e7f430273590cf8f230eb0ae0b86bfe
AdiG <adigollamudi@gmail.com>
2020-12-31 12:07:19 -0500

    images file, snapshot boxes and icons

--- client/homepage/Homepage.jsx
+++ client/homepage/Homepage.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './homepage.scss';
+import comprIcon from '../images/measuring-tool.jpg';
+import help from '../images/alt-help-icon.jpg';
+import info from '../images/info-page.jpg';
+
 
 class Homepage extends Component {
   render() {
     return (
       <div>
         <div id="welcome-banner">Welcome To EKY Coding!</div>
+        <div className="program-glimpse" id="comprehensive">Comprehensive Lessons</div>
+        <img src={comprIcon} id="compre-img" className="img" />
+        <div className="program-glimpse" id="help">Easy Access To Help</div>
+        <img src={help} id="help-icon" className="img" />
+        <div className="program-glimpse" id="curriculum">Immersive Curriculum</div>
+        <img src={info} id="info-page" className="img" />
       </div>
     );
   }
