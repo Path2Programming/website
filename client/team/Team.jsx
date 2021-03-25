4fa448e7fd0e327dd3d044143292ee706283a215
AdiG <adigollamudi@gmail.com>
2021-03-24 23:20:00 -0400

    white divs with temp profile pics

--- client/team/Team.jsx
+++ client/team/Team.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
-class Team extends Component {
-  render() {
-    return (
-      <div>
-        <p>Team</p>
+import './team.scss';
+
+function Team() {
+  return (
+    <div>
+      <div className="profile" id="Adi">
+        {/* <img src="../images/tempProfilePic.png" id="Adipfp"></img> */}
+        <input className="pfp" type="button"></input>
+        <p>Aditya Gollamudi, Founder and Executive Director</p>
       </div>
-    );
-  }
+
+      <div className="profile" id="Arthi">
+        {/* <img src="../images/tempProfilePic.png" className="pfp"></img> */}
+        <input className="pfp" type="button"></input>
+        <p>Arthi</p>
+      </div>
+
+      <div className="profile" id="Pranav">
+        {/* <img src="../images/tempProfilePic.png" className="pfp"></img> */}
+        <input className="pfp" type="button"></input>
+        <p>Pranav</p>
+      </div>
+    </div>
+  )
 }
 
 export default Team;
