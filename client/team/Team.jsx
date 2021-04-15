178ab48a92a861c84382875e4cae3f9432c91ea1
AdiG <adigollamudi@gmail.com>
2021-04-15 17:26:27 -0400

    removed Arthi

--- client/team/Team.jsx
+++ client/team/Team.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import AdiPfp from '../images/AdiPfp.jpeg';
+import PranavPfp from '../images/PranavPfp.jpeg';
 
 import './team.scss';
 
 function Team() {
   return (
-    <div>
+    <>
       {/* <h1 id="header">Meet The Team</h1> */}
 
-      <div id="members"></div>
+      <div id="members">
+        <div className="cards">
+          <div className="profile" id="Adi">
+            {/* <div className="pfp" id="adi-pfp"></div> */}
+            <img src={AdiPfp} className="pfp" />
+            <p>Aditya Gollamudi</p>
+            <p id="title">Founder and Executive Director</p>
 
-      <div className="profile" id="Adi">
-        <div className="pfp" id="adi-pfp"></div>
-        <p>Aditya Gollamudi</p>
-        <p id="title">Founder and Executive Director</p>
-
-        <a href="https://www.linkedin.com/in/aditya-gollamudi/" target="_blank" rel="noreferrer">
+            {/* <a href="https://www.linkedin.com/in/aditya-gollamudi/" target="_blank" rel="noreferrer">
           <input className="linkedIn" id="AdiLI" type="button"></input>
         </a>
 
@@ -24,33 +27,28 @@ function Team() {
 
         <a href="mailto:adigollamudi@gmail.com">
           <input className="gmail" id="adi-gmail" type="button"></input>
-        </a>
-      </div>
+        </a> */}
+          </div>
 
-      <div className="profile" id="Arthi">
-        <div className="pfp" id="arthi-pfp"></div>
-        <p>Arthi Sundar</p>
-        <p id="title">Growth and Marketing</p>
-
-        <a href="mailto:Arthibot@gmail.com">
-          <input className="gmail" id="arthi-gmail" type="button"></input>
-        </a>
-      </div>
-
-      <div className="profile" id="Pranav">
-        <div className="pfp" id="pranav-pfp"></div>
-        <p>Pranav Gangumolu</p>
-        <p id="title">Head of Logistics</p>
-
+          <div className="profile" id="Pranav">
+            {/* <div className="pfp" id="pranav-pfp"></div> */}
+            <img src={PranavPfp} className="pfp" />
+            <p>Pranav Gangumolu</p>
+            <p id="title">Head of Logistics</p>
+            {/* 
         <a href="https://www.instagram.com/theofficial_pranav/" target="_blank" rel="noreferrer">
           <input className="instagram" id="pranav-instagram" type="button"></input>
         </a>
 
         <a href="mailto:pranavgangumolu@gmail.com">
           <input className="gmail" id="pranav-gmail" type="button"></input>
-        </a>
+        </a> */}
+          </div>
+        </div>
+
       </div>
-    </div>
+
+    </>
   )
 }
 
