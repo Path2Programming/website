6cd086701fdd642180fe26b26184c086bb7b6253
Adi-Goll <adigollamudi@gmail.com>
2021-11-23 09:03:44 -0500

    aligned footer to center

--- client/footer/Footer.jsx
+++ client/footer/Footer.jsx
@@ -9,17 +9,17 @@ class Footer extends Component {
                     <div className="row">
                         {/* COLUMN 1 */}
                         <div className="col">
-                            <h4>More Info</h4>
                             <ul className="list-unstyled">
+                                <h4>Questions? Please contact:</h4>
                                 <li>aditya@path2programming.com</li>
                                 <li>502-775-9203</li>
-                                <li>item 3</li>
+                                <li>look on the website more</li>
                             </ul>
                         </div>
                         {/* COLUMN 2 */}
                         <div className="col">
-                            <h4>Column 2</h4>
                             <ul className="list-unstyled">
+                                <h4>Column 2</h4>
                                 <li>testing</li>
                                 <li>i'm so good at this</li>
                                 <li>item 3</li>
