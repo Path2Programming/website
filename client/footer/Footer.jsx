98c249d08aea1c59b7e79fb90f16281e888cdf42
Adi-Goll <adigollamudi@gmail.com>
2022-04-10 01:31:25 -0400

    basic footer with logo and contact info

--- client/footer/Footer.jsx
+++ client/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './footer.scss';
+import '../images/logo.jpg';
 
 class Footer extends Component {
     render() {
@@ -9,25 +10,19 @@ class Footer extends Component {
                     <div className="row">
                         {/* COLUMN 1 */}
                         <div className="col">
-                            <ul className="list-unstyled">
-                                <h4>Questions? Please contact:</h4>
-                                <li>aditya@path2programming.com</li>
-                                <li>502-775-9203</li>
-                                <li>look on the website more</li>
-                            </ul>
+                            <div id="logo"></div>
                         </div>
                         {/* COLUMN 2 */}
                         <div className="col">
                             <ul className="list-unstyled">
-                                <h4>Column 2</h4>
-                                <li>testing</li>
-                                <li>i'm so good at this</li>
-                                <li>item 3</li>
+                                <li>Contact Us:</li>
+                                <li>aditya@path2programming.com</li>
+                                <li>502-775-9203</li>
                             </ul>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
