f2b14e9da372bce0287cabd0bcd079394bf78169
Adi-Goll <adigollamudi@gmail.com>
2022-04-11 23:22:57 -0400

    fixed scroll overflow, set absolute footer position, and embedded intro vid on homepage

--- client/homepage/Homepage.jsx
+++ client/homepage/Homepage.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import './homepage.scss';
 
 class Homepage extends Component {
   render() {
     return (
       <div>
-        <p>Homepage</p>
+        <iframe className="YouTube-video" width="560" height="315" src="https://www.youtube.com/embed/pQ6UqbNJWjQ"
+          title="YouTube video player" frameborder="0" allow="accelerometer; 
+        autoplay; clipboard-write; encrypted-media; gyroscope; 
+        picture-in-picture; fullscreen" ></iframe>
       </div>
     );
   }
