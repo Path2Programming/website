d76329515eeaa92ddfa1c5ec43e2520903792c72
Adi-Goll <adigollamudi@gmail.com>
2022-06-22 01:02:27 -0400

    added flower header to homepage with scroll effect

--- client/homepage/Homepage.jsx
+++ client/homepage/Homepage.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import './homepage.scss';
+import Kentucky from '../images/kentucky-map.png';
+import Bridge from '../images/bridge-in-forest.jpg';
 
 class Homepage extends Component {
   render() {
     return (
       <div>
-        <iframe className="YouTube-video" width="560" height="315" src="https://www.youtube.com/embed/pQ6UqbNJWjQ"
-          title="YouTube video player" frameborder="0" allow="accelerometer; 
-        autoplay; clipboard-write; encrypted-media; gyroscope; 
-        picture-in-picture; fullscreen" ></iframe>
+
+
+        <div className='header' alt="Kentucky Mountains" div></div>
+
+        <div>
+
+          <iframe className="YouTube-video" width="560" height="315" src="https://www.youtube.com/embed/pQ6UqbNJWjQ"
+            title="YouTube video player" frameborder="0" allow="accelerometer; 
+          autoplay; clipboard-write; encrypted-media; gyroscope; 
+          picture-in-picture; fullscreen" ></iframe>
+
+          <h2 id="tutorials">Tutorials</h2>
+          <div id="vid-description">
+            P2P offers a collection of programming tutorials for free on YouTube. The videos cover a
+            variety of topics including Python and web development with more to come!
+          </div>
+
+
+
+        </div>
       </div>
     );
   }
