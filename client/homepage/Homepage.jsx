5e61e213da0ba4583e1e7db07a5a07b39982ae7c
Adi-Goll <adigollamudi@gmail.com>
2022-06-22 03:07:03 -0400

    words on header image and sticky sidebar

--- client/homepage/Homepage.jsx
+++ client/homepage/Homepage.jsx
@@ -8,14 +8,16 @@ class Homepage extends Component {
       <div>
 
 
-        <div className='header' alt="Kentucky Mountains" div></div>
+        <div id="container">
+          <div className='header' alt="White Flowers"></div>
+          <div id="center-text">Revamping Tech Education in Kentucky</div>
+        </div>
 
         <div>
 
-          <iframe className="YouTube-video" width="560" height="315" src="https://www.youtube.com/embed/pQ6UqbNJWjQ"
-            title="YouTube video player" frameborder="0" allow="accelerometer; 
-          autoplay; clipboard-write; encrypted-media; gyroscope; 
-          picture-in-picture; fullscreen" ></iframe>
+          <iframe className="YouTube-video" width="560" height="315" src="https://www.youtube.com/embed/p0oxHoGjVWg"
+            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+          clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
           <h2 id="tutorials">Tutorials</h2>
           <div id="vid-description">
